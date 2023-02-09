cb89ed6bc238418949684eb8042ee87306e934fc
lapyae <lapyae@lapyaes-MacBook-Air.local>
2023-02-09 15:34:28 +0630

    beauty category add

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+
+    public function Index(){
+        //BEAUTY
+        $skip_category_0 =  Category::skip(0)->first();
+        $skip_product_0 = Product::where('status',1)
+        ->where('category_id',$skip_category_0->id)
+        ->orderBy('id','desc')
+        ->get();
+        //END
+        return view('frontend.index',compact('skip_category_0','skip_product_0'));
+    }
     public function productDetails($id,$slug){
         $product = Product::findOrFail($id);
         $product_colours = explode(',',$product->product_color);
--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('frontend.index');
-});
+// Route::get('/', function () {
+//     return view('frontend.index');
+// });
 
+Route::get('/',[IndexController::class,'Index']);
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
